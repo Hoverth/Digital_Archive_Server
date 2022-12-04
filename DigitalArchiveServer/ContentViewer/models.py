857ad8fe88466d7091f4df650bf0a412bb4f882e
Thomas Dickson <modscrat@gmail.com>
2022-12-05 11:26:15 +1300

    Create ContentViewer models, add ContentViewer to INSTALLED_APPS in settings.py

--- DigitalArchiveServer/ContentViewer/models.py
+++ DigitalArchiveServer/ContentViewer/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Tag(models.Model):
+    # Standard Fields
+    name = models.CharField(max_length=255)
+    tag_id = models.CharField(max_length=255, null=True)
+    source_url = models.CharField(max_length=255, null=True)
+    source_id = models.CharField(max_length=255, null=True)
+
+    # Additional Fields
+    preview = models.CharField(max_length=512, default='<p class=\'preview\'>This tag has no generated preview</p>')
+
+
+class Creator(models.Model):
+    # Standard Fields
+    name = models.CharField(max_length=255)
+    creator_id = models.CharField(max_length=255, null=True)
+    about = models.TextField(null=True)
+    source_url = models.CharField(max_length=255, null=True)
+    source_id = models.CharField(max_length=255, null=True)
+
+    # Additional Fields
+    related_creators = models.ManyToManyField('self')
+    preview = models.CharField(max_length=512, default='<p class=\'preview\'>This creator has no generated preview</p>')
+
+
+class Content(models.Model):
+    # Standard Fields
+    title = models.CharField(max_length=255)
+    content_type = models.CharField(max_length=255)
+    source_url = models.CharField(max_length=255)
+    source_id = models.CharField(max_length=255)
+    content_path = models.CharField(max_length=255)
+    time_retrieved = models.DateTimeField(default=timezone.now)
+    creators = models.ManyToManyField('Creator', related_name='content_creators')
+    tags = models.ManyToManyField('Tag', related_name='content_tags')
+    notes = models.TextField(null=True)
+    time_created = models.DateTimeField(null=True)
+    language = models.CharField(max_length=255, null=True)
+    copyright = models.CharField(max_length=255, null=True)
+    related_content = models.ManyToManyField('self')
+
+    # Additional Fields
+    published_date = models.DateTimeField(auto_now_add=True)
+    last_modified_date = models.DateTimeField(auto_now=True)
+    content_size = models.PositiveIntegerField()
+    seen_by = models.ManyToManyField(User, related_name='content_seen_by')
+    liked_by = models.ManyToManyField(User, related_name='content_liked_by')
+    preview = models.CharField(max_length=512, default='<p class=\'preview\'>This content has no generated preview</p>')
+
+
+class Collection(models.Model):
+    related_collections = models.ManyToManyField('self')
+    preview = models.CharField(max_length=512, default='<p class=\'preview\'>This collection has no generated preview</p>')
