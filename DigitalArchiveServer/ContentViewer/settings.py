566c7f13243965348cc9df31536c572fabc223c9
Thomas Dickson <modscrat@gmail.com>
2022-12-08 10:07:48 +1300

    tweak the storage dir path

--- DigitalArchiveServer/ContentViewer/settings.py
+++ DigitalArchiveServer/ContentViewer/settings.py
@@ -1,2 +1,2 @@
 # this directory is where the static files are served from, that contains all the content
-STORAGE_DIR = "/home/organiser/staticfiles"
+STORAGE_DIR = "/home/digital-archive-server/staticfiles"
