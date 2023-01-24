aa8353dffc76724897722f1e317201e2f4c947d1
Thomas Dickson <modscrat@gmail.com>
2023-01-24 21:54:58 +1300

    attempt to optimise search function by not querying for empty values

--- DigitalArchiveServer/ContentManager/search.py
+++ DigitalArchiveServer/ContentManager/search.py
@@ -1,3 +1,6 @@
+import operator
+from functools import reduce
+
 from django import forms
 from django.db.models import Q
 from django.views import generic
@@ -56,15 +59,39 @@ class SearchView(generic.ListView):
         if self.request.method == 'GET':
             form = SearchForm(self.request.GET)
             if form.is_valid():
-                print(form.cleaned_data)
+                q_list = []
+
                 title = form.cleaned_data['title']
+                if title != '':
+                    q_list.append(Q(title__icontains=title))
+
                 content_type = form.cleaned_data['content_type']
+                if content_type != '':
+                    q_list.append(Q(content_type__icontains=content_type))
+
                 source_url = form.cleaned_data['source_url']
+                if source_url != '':
+                    q_list.append(Q(source_url__icontains=source_url))
+
                 source_id = form.cleaned_data['source_id']
+                if source_id != '':
+                    q_list.append(Q(source_id__icontains=source_id))
+
                 content_path = form.cleaned_data['content_path']
+                if content_path != '':
+                    q_list.append(Q(content_path__icontains=content_path))
+
                 notes = form.cleaned_data['notes']
+                if notes != '':
+                    q_list.append(Q(notes__icontains=notes))
+
                 language = form.cleaned_data['language']
+                if language != '':
+                    q_list.append(Q(language__icontains=language))
+
                 copyright = form.cleaned_data['copyright']
+                if copyright != '':
+                    q_list.append(Q(copyright=copyright))
 
                 time_created_start = form.cleaned_data['time_created_start']
                 time_created_end = form.cleaned_data['time_created_end']
@@ -76,54 +103,38 @@ class SearchView(generic.ListView):
                 content_size = form.cleaned_data['content_size']
 
                 from_archiver = form.cleaned_data['from_archiver']
+                if from_archiver != '':
+                    q_list.append(Q(from_archiver__name__icontains=from_archiver))
+
                 creator_names = form.cleaned_data['creator_names']
+                if from_archiver != '':
+                    q_list.append(Q(creators__name__icontains=creator_names))
+
                 tag_names = form.cleaned_data['tag_names']
+                if from_archiver != '':
+                    q_list.append(Q(tags__name__icontains=tag_names))
+
 
                 if content_size is not None:
-                    content_filter = Q(content_size__gt=(content_size - 50)) & Q(content_size__lt=(content_size + 50))
-                else:
-                    content_filter = Q()
+                    q_list.append(Q(content_size__gt=(content_size - 50)) & Q(content_size__lt=(content_size + 50)))
 
                 if time_created_start is not None and time_created_end is not None:
-                    time_created_filter = Q(time_retrieved__range=(time_created_start, time_created_end))
-                else:
-                    time_created_filter = Q()
+                    q_list.append(Q(time_retrieved__range=(time_created_start, time_created_end)))
 
                 if time_retrieved_start is not None and time_retrieved_end is not None:
-                    time_retrieved_filter = Q(time_retrieved__range=(time_retrieved_start, time_retrieved_end))
-                else:
-                    time_retrieved_filter = Q()
+                    q_list.append(Q(time_retrieved__range=(time_retrieved_start, time_retrieved_end)))
 
                 if published_date_start is not None and published_date_end is not None:
-                    published_date_filter = Q(time_retrieved__range=(published_date_start, published_date_end))
-                else:
-                    published_date_filter = Q()
+                    q_list.append(Q(time_retrieved__range=(published_date_start, published_date_end)))
 
                 order_by = '-time_retrieved'
                 if form.cleaned_data['order_by'] != '':
                     order_by = form.cleaned_data['order_by']
-                print(order_by)
 
-                content_list = Content.objects.filter(
-                    Q(title__icontains=title) &
-                    Q(content_type__icontains=content_type) &
-                    Q(source_url__icontains=source_url) &
-                    Q(source_id__icontains=source_id) &
-                    Q(content_path__icontains=content_path) &
-                    Q(notes__icontains=notes) &
-                    Q(language__icontains=language) &
-                    Q(copyright__icontains=copyright) &
-
-                    time_created_filter &
-                    time_retrieved_filter &
-                    published_date_filter &
-
-                    content_filter &
-
-                    Q(from_archiver__name__icontains=from_archiver) &
-                    Q(creators__name__icontains=creator_names) &
-                    Q(tags__name__icontains=tag_names)
-                ).distinct().order_by(order_by)
+                if q_list:
+                    content_list = Content.objects.filter(reduce(operator.and_, q_list)).distinct().order_by(order_by)
+                else:
+                    content_list = Content.objects.all().order_by(order_by)
 
                 if not self.request.user.is_authenticated or not self.request.user.has_perm('ContentManager.adult_content'):
                     content_list = content_list.exclude(tags__tag_id='adult-content')
